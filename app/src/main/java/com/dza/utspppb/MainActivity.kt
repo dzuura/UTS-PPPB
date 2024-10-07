cbab6f11a7bdf59b506128755f247d3c5d9e12b2
dzuura <daffa3704@gmail.com>
2024-10-07 11:11:20 +0700

    Initial commit

---
+++ app/src/main/java/com/dza/utspppb/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.dza.utspppb
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dza.utspppb.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.profil.setOnClickListener {
+            val intent = Intent(this@MainActivity, SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/dza/utspppb/SecondActivity.kt
@@ -0,0 +1,23 @@
+package com.dza.utspppb
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dza.utspppb.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySecondBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnBack.setOnClickListener {
+            val intent = Intent(this@SecondActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
